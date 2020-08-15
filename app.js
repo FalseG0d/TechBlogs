95bc91bec7b54b0e794522c0fab710fd5a206a4c
Apoorv Garg <u.garg.10@gmail.com>
2020-08-15 20:56:33 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const path = require('path')
+
+const passport = require('passport')
+
+const session = require('express-session')
+
+const morgan = require('morgan')
+
+const exphbs = require('express-handlebars')
+const connectDB = require('./config/db')
+
+const mongoose = require('mongoose')
+const MongoStore = require('connect-mongo')(session)
+
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+const app = express()
+
+//Body Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+//Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//Passport Config
+require('./config/passport')(passport)
+
+//Static
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
+
+//Handlebars
+app.engine('.hbs', exphbs({
+    helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+    },
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs')
+
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
+
+//Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Set Global Variable
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
+
+//Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
